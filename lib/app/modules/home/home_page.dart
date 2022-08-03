4aa37149fb3d10232272ee2f9c98a2d536c02051
Jcarlos1982 <jcarlossilva@outlook.com>
2022-08-03 01:08:12 -0300

    flavors refactoring

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:app_flavors/flavors/flavors.dart';
+import 'package:app_flavors/flavors/config_flavors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
--- lib/main_flavor.dart
+++ lib/main_flavor.dart
@@ -1,6 +1,6 @@
 import 'package:app_flavors/app/app_module.dart';
 import 'package:app_flavors/app/app_widget.dart';
-import 'package:app_flavors/flavors/flavors.dart';
+import 'package:app_flavors/flavors/config_flavors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
